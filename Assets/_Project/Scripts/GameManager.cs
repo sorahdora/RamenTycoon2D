bad744c80d9bc952627320f640df1494839630b8
sorahdora <sorahdora@gmail.com>
2026-07-29 22:18:54 -0400

    1

---
+++ Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TMP_Text timerText;
+    public TMP_Text moneyText;
+
+    public float gameDuration = 60f;
+    public int targetMoney = 10;
+
+    private float remainingTime;
+    private int currentMoney = 0;
+    private bool gameEnded = false;
+
+    void Start()
+    {
+        remainingTime = gameDuration;
+        UpdateMoneyText();
+        UpdateTimerText();
+    }
+
+    void Update()
+    {
+        if (gameEnded)
+        {
+            return;
+        }
+
+        remainingTime -= Time.deltaTime;
+
+        if (remainingTime <= 0f)
+        {
+            remainingTime = 0f;
+            EndGame();
+        }
+
+        UpdateTimerText();
+    }
+
+    void UpdateTimerText()
+    {
+        int seconds = Mathf.CeilToInt(remainingTime);
+        timerText.text = "TIME: " + seconds;
+    }
+
+    void UpdateMoneyText()
+    {
+        moneyText.text = "MONEY: $" + currentMoney;
+    }
+
+    public void AddMoney(int amount)
+    {
+        if (gameEnded)
+        {
+            return;
+        }
+
+        currentMoney += amount;
+        UpdateMoneyText();
+    }
+
+    void EndGame()
+    {
+        gameEnded = true;
+
+        if (currentMoney >= targetMoney)
+        {
+            Debug.Log("YOU WIN!");
+        }
+        else
+        {
+            Debug.Log("YOU LOSE!");
+        }
+    }
+}
